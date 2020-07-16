d83e6ca22cae3a4229f5fc7322bfd245710820e6
Austin Ng <austinng98@gmail.com>
2020-07-15 17:54:58 -0700

    finished timer

--- A/main.c
+++ A/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include "sort.h"
 
@@ -14,15 +15,29 @@ int *start2;
 int main(int argc, char* argv[]) {
     FILE *in;
     FILE *out;
-    int temp, start, i;
+    int temp, startIndex, i;
     pthread_t tid1, tid2, tid3;
-    start = 0;
+    struct timespec start, finish;
+    double elapsed;
+    clock_gettime(CLOCK_MONOTONIC, &start);
 
+    startIndex = 0;
+
+    if (argc != 2) {
+        printf("Usage: ./sort [inputFile]\n");
+        exit(EXIT_FAILURE);
+    }
     /* open input file and fill arr */
     in = fopen(argv[1], "r");
+    if (!in) {
+        printf("Error: '%s' not found\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
     while (fscanf(in, "%d", &temp) == 1) {
         arr[len++] = temp;
     }
+    fclose(in);
 
     /* assign global variables */
     start1 = arr;
@@ -44,10 +59,17 @@ int main(int argc, char* argv[]) {
 
     /* write sorted array to output */
     out = fopen("out.txt", "w");
-    for (i = start; i < len; i++) {
+    for (i = startIndex; i < len; i++) {
         fprintf(out, "%d ", res[i]);
     }
     fprintf(out, "\n");
+    fclose(out);
+
+    /* print time */
+    clock_gettime(CLOCK_MONOTONIC, &finish);
+    elapsed = (finish.tv_sec - start.tv_sec);
+    elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+    printf("Finished in %f seconds.\n", elapsed);
 
     return 0;
 }
