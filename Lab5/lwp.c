ac8a623c027a311f97c69a9fc322b28605b77409
Kelechi Igwe <kigwe@unix1.csc.calpoly.edu>
2020-08-01 17:55:00 -0700

    Changes to lwp_create

--- Lab5/lwp.c
+++ Lab5/lwp.c
@@ -3,13 +3,27 @@
 #include "fp.h"
 #include "lwp.h"
 
+
+#define NUM_THREADS 64
+
+
 static tid_t next_tid = 0; /* Thread ID value to set new thread to */
+static int thread_len = 0; /* Length of threads buffer */
+static int cur_max_threads = NUM_THREADS; /* Dynamic max number of threads */
+static int running_threads = 0; /* Number of threads that are running */
+thread* threads; /* Buffer holding created threads */
+
 
 tid_t lwp_create(lwpfun fun, void* args, size_t ssize) {
     /* Takes a function pointer (fun), a list of arguments (args), and
      * a stack size (ssize) and creates a new lightweight process (LWP).
      * Returns the thread id of the new LWP
      */
+
+    #ifndef INIT_ARRAY /* Malloc the array of threads if not already done */
+	#define INIT_ARRAY
+	threads = malloc(NUM_THREADS);
+    #endif
 
     unsigned long sp; /* Base stack pointer */
     int count;
@@ -20,11 +34,14 @@ tid_t lwp_create(lwpfun fun, void* args, size_t ssize) {
 
     /* Push data to the stack */
     GetSP(sp);
-    push(sp, lwp_exit());
-    push(sp, 0xdeadbeef);
+
+    sp--;
+    SetSP(lwp_exit); /* Return Address */
+    sp--;
+    SetSP(0xdeadbeef); /* Required fluff */
 
     /* Set Thread ID */
-    sync_and_fetch(&next_tid, 1);
+    __sync_fetch_and_add(&next_tid, 1);
     newthread->tid = next_tid;
 
     /* Set Stack Pointer and Stack Size */
@@ -61,25 +78,41 @@ tid_t lwp_create(lwpfun fun, void* args, size_t ssize) {
 		break;
 	}
 
+	count++;
         args++;
     }
 
     newthread->state.fxsave = FPU_INIT; /* Store intital FPU state */
 
     /* Admit the new LWP into the scheduler */
-    sched = lwp_get_scheduler();
+    scheduler sched = lwp_get_scheduler();
     sched->admit(newthread);
+
+    /* Updating outer scope variables */
+    __sync_fetch_and_add(&running_threads, 1);
+    __sync_fetch_and_add(&thread_len, 1);
+
+    /* Add new thread to threads buffer */
+    if (thread_len == cur_max_threads) {
+	cur_max_threads += NUM_THREADS;
+	threads = realloc(threads, cur_max_threads);
+    }
+
+    threads[newthread->tid - 1] = newthread; 
 
     return newthread->tid;
 }
+
 
 void lwp_exit() {
     /* Terminates the LWP that called this function */
 }
 
+
 tid_t lwp_gettid() {
     /* Returns the thread ID of the LWP that called this function */
 }
+
 
 void lwp_yield() {
     /* Saves the context of the LWP that called this function and stops
@@ -87,13 +120,16 @@ void lwp_yield() {
      */
 }
 
+
 void lwp_start() {
     /* Starts the LWP that called this function */
 }
 
+
 void lwp_stop() {
     /* Stops the LWP that called this function */
 }
+
 
 void lwp_set_scheduler(scheduler fun) {
     /* Takes a new scheduling function (fun) and sets it as the current
@@ -101,9 +137,11 @@ void lwp_set_scheduler(scheduler fun) {
      */
 }
 
+
 scheduler lwp_get_scheduler() {
     /* Returns the current scheduling function being ran */
 }
+
 
 thread tid2thread(tid_t tid) {
     /* Takes a thread ID (tid) and returns the thread that is mapped to that
@@ -111,7 +149,7 @@ thread tid2thread(tid_t tid) {
      */
 
     if ((tid < next_tid) && (tid > NO_THREAD)) {
-	return NULL; /*FIX THIS TO BE THE CALLING LWP*/
+	return threads[tid-1];
     }
     else { /* Invalid Thread ID */
         return NULL;
