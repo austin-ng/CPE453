4021d1ee7c95893c9eaa796a89299cc4fa228d4a
Kelechi Igwe <kigwe@unix1.csc.calpoly.edu>
2020-08-20 15:05:46 -0700

    Lab 7 pre-pre-alpha files

---
+++ Lab7/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "vm_manager.h"
+
+int main(int argc, char* argv[]) {
+    printf("Hey how's it going %s\n", PLACEHOLDER);
+
+    return 0;
+}
---
+++ Lab7/vm_manager.h
@@ -0,0 +1,6 @@
+#ifndef VM_MANAGER
+#define VM_MANAGER
+
+#define PLACEHOLDER "BOB"
+
+#endif /* VM_MANAGER_H */
